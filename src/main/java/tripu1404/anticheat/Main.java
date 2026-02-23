f4ec9992207faab60a112afef391e22c1567145e
Tripu1404 <121175509+Tripu1404@users.noreply.github.com>
2026-02-23 00:21:39 -0300

    Update Main.java

--- src/main/java/tripu1404/anticheat/Main.java
+++ src/main/java/tripu1404/anticheat/Main.java
@@ -1,6 +1,7 @@
 package tripu1404.anticheat;
 
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
 import tripu1404.anticheat.checks.InventoryMoveCheck;
 import tripu1404.anticheat.checks.NoSlowDownCheck;
 
@@ -8,10 +9,23 @@ public class Main extends PluginBase {
 
     @Override
     public void onEnable() {
-        // Registramos las clases separadas como Listeners
-        this.getServer().getPluginManager().registerEvents(new InventoryMoveCheck(), this);
-        this.getServer().getPluginManager().registerEvents(new NoSlowDownCheck(), this);
+        // Genera el archivo config.yml en la carpeta del plugin si no existe
+        this.saveDefaultConfig();
         
-        this.getLogger().info("AntiCheat activado: Checks cargados correctamente.");
+        // Obtenemos la configuración
+        Config config = this.getConfig();
+
+        // Cargamos el check de Inventory Move si está activado
+        if (config.getBoolean("checks.inventory-move", true)) {
+            this.getServer().getPluginManager().registerEvents(new InventoryMoveCheck(), this);
+        }
+
+        // Cargamos el check de NoSlowDown si está activado
+        if (config.getBoolean("checks.no-slow-down", true)) {
+            this.getServer().getPluginManager().registerEvents(new NoSlowDownCheck(), this);
+        }
+        
+        // Único mensaje que se enviará a la consola al iniciar
+        this.getLogger().info("§aAntiCheat activado correctamente.");
     }
 }
